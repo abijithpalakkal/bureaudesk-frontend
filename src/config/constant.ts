d7bc16b8f6a8da9c3e9773340f40aad8de8ed4d9
Abijith P <abhijithunni00@gmail.com>
2024-08-30 19:40:24 +0530

    changed env of baseurl

--- src/config/constant.ts
+++ src/config/constant.ts
@@ -1,3 +1,3 @@
 // export const baseUrl = 'https://bureaudesk.futstore.me'
-export const baseUrl = import.meta.env.base_url
+export const baseUrl = import.meta.env.VITE_REACT_APP_BASE_URL;
 export const SOCKET_URL = import.meta.env.VITE_REACT_APP_SOCKET_URL
